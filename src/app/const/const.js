a7ed9942db0cabff2264f7285a84d63b7d425f92
Abdullah Ahmad Hafiz <hafiz@192.168.1.6>
2025-01-04 10:07:25 +0700

    Fix URL

--- src/app/const/const.js
+++ src/app/const/const.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = "https://www.runtah.id/operate/api";
-export const API_BASE_URL = "http://localhost/operate/api";
+export const API_BASE_URL = "http://localhost:4000/api";
 // export const API_MOBILE_URL = "https://www.runtah.id/operate";
